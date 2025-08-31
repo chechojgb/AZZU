6cf366da85c15400b53100ba13e42ab8cc55d39f
chechojgb <s.ortiz@montechelo.com.co>
2025-08-30 22:35:16 -0500

    add: nuevos componentes dashboard
    Se pasaron los componentes maquetados a funcionar correctamente, solo falta el analisis historico por semana.

--- resources/js/components/BL/MovimientosRecientes.jsx
+++ resources/js/components/BL/MovimientosRecientes.jsx
@@ -1,52 +1,53 @@
 import { ArrowDownCircle, ArrowUpCircle } from 'lucide-react';
 
-const movimientos = [
-  {
-    tipo: 'Entrada',
-    producto: 'Botón Z6',
-    cantidad: 500,
-    fecha: '2025-08-02',
-    icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
-  },
-  {
-    tipo: 'Salida',
-    producto: 'Botón R4',
-    cantidad: 300,
-    fecha: '2025-08-02',
-    icono: <ArrowUpCircle className="text-red-600 w-5 h-5" />,
-  },
-  {
-    tipo: 'Entrada',
-    producto: 'Botón L2',
-    cantidad: 200,
-    fecha: '2025-08-01',
-    icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
-  },
-  {
-    tipo: 'Salida',
-    producto: 'Botón M1',
-    cantidad: 100,
-    fecha: '2025-07-31',
-    icono: <ArrowUpCircle className="text-red-600 w-5 h-5" />,
-  },
-  {
-    tipo: 'Entrada',
-    producto: 'Botón Z1',
-    cantidad: 200,
-    fecha: '2025-08-01',
-    icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
-  },
-  {
-    tipo: 'Entrada',
-    producto: 'Botón Z1',
-    cantidad: 1000,
-    fecha: '2025-08-02',
-    icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
-  },
-  // Puedes duplicar más para probar el scroll
-];
+// const movimientos = [
+//   {
+//     tipo: 'Entrada',
+//     producto: 'Botón Z6',
+//     cantidad: 500,
+//     fecha: '2025-08-02',
+//     icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
+//   },
+//   {
+//     tipo: 'Salida',
+//     producto: 'Botón R4',
+//     cantidad: 300,
+//     fecha: '2025-08-02',
+//     icono: <ArrowUpCircle className="text-red-600 w-5 h-5" />,
+//   },
+//   {
+//     tipo: 'Entrada',
+//     producto: 'Botón L2',
+//     cantidad: 200,
+//     fecha: '2025-08-01',
+//     icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
+//   },
+//   {
+//     tipo: 'Salida',
+//     producto: 'Botón M1',
+//     cantidad: 100,
+//     fecha: '2025-07-31',
+//     icono: <ArrowUpCircle className="text-red-600 w-5 h-5" />,
+//   },
+//   {
+//     tipo: 'Entrada',
+//     producto: 'Botón Z1',
+//     cantidad: 200,
+//     fecha: '2025-08-01',
+//     icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
+//   },
+//   {
+//     tipo: 'Entrada',
+//     producto: 'Botón Z1',
+//     cantidad: 1000,
+//     fecha: '2025-08-02',
+//     icono: <ArrowDownCircle className="text-green-600 w-5 h-5" />,
+//   },
+//   // Puedes duplicar más para probar el scroll
+// ];
 
-export default function MovimientosRecientesBL() {
+export default function MovimientosRecientesBL({movimientos}) {
+  console.log('movimientos',movimientos);
   return (
     <div className="relative flex flex-col h-full p-5">
       {/* Header */}
@@ -57,23 +58,35 @@ export default function MovimientosRecientesBL() {
       {/* Lista scrollable */}
       <div className="flex-1 overflow-y-auto pr-1">
         <ul className="space-y-4 text-sm text-gray-800 dark:text-gray-300">
-          {movimientos.map((mov, i) => (
-            <li key={i} className="flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-full">
-                  {mov.icono}
+          {Object.values(movimientos).map((mov, i) => {
+            let icono;
+            if (mov.tipo === "entrada") {
+              icono = <ArrowUpCircle className="text-green-600 w-5 h-5" />;
+            } else {
+              icono = <ArrowDownCircle className="text-red-600 w-5 h-5" />;
+            }
+
+            return (
+              <li key={i} className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <div className="bg-gray-100 dark:bg-gray-800 p-2 rounded-full">
+                    {icono}
+                  </div>
+                  <div>
+                    <p className="font-medium">
+                      {mov.tipo}: {mov?.empaque?.producto?.descripcion}
+                    </p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                      Fecha: {mov.created_at.slice(0, 10)}
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  <p className="font-medium">{mov.tipo}: {mov.producto}</p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    Cantidad: {mov.cantidad}
-                  </p>
-                </div>
-              </div>
-              <div className="text-xs text-gray-500 dark:text-gray-400">{mov.fecha}</div>
-            </li>
-          ))}
+                <div className="text-xs text-gray-500 dark:text-gray-400">{mov.fecha}</div>
+              </li>
+            );
+          })}
         </ul>
+
       </div>
     </div>
   );
